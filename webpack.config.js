df1afeeeec11677df6ed61dcaaccb2e1368fb322
ibritvic <ivbritvic@gmail.com>
2021-09-28 10:02:25 +0200

    Chrome inline style CSP extension bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path'),
+      webpack = require('webpack'),
       CopyWebpackPlugin = require('copy-webpack-plugin');
+      
 
 module.exports = {
     mode: "development",
@@ -8,11 +10,11 @@ module.exports = {
     entry: {
         content: './src/app/content.ts',
         background: './src/app/background.ts',
-        popup: './src/pages/popup.tsx',
+        popup: './src/pages/popup.tsx'
     },
 
     output: {
-        path: path.resolve(__dirname, 'dist/'),
+        path: path.resolve(__dirname, 'dist/js'),
         filename: '[name].js'
     },
 
@@ -30,9 +32,14 @@ module.exports = {
     plugins: [
         new CopyWebpackPlugin({
           patterns: [
-              { from: 'src/pages/*.html'},
-              { from: 'src/icons', to: './icons'},
+              { from: 'src', to:'../', globOptions: { ignore: [ '**.ts', '**.tsx' ] }} 
           ]
-        })
+        }),
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined            
+            INLINE_RUNTIME_CHUNK:false, // To prevent Chrome CSP error, doesn't work see .env file
+            DEBUG: false,
+          })
+          
     ]
 };
